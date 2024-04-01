62adb202e252af3e1bcf14b4ea74205aa6510014
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2024-04-01 15:17:34 +0200

    Intergrated Blog backend

--- UI/Dashbord/dashbordBlog.js
+++ UI/Dashbord/dashbordBlog.js
@@ -1,4 +1,5 @@
-let allBlogs = JSON.parse(localStorage.getItem('allTheBlogs')) || [];
+let allBlogs;
+let totalBlogs = 0;
 
   const editBlogs = document.querySelector('.editBlog');
   const rightElement = document.querySelector('.rightSide');
@@ -23,46 +24,75 @@ let allBlogs = JSON.parse(localStorage.getItem('allTheBlogs')) || [];
     leftElement.style.filter = 'grayscale(0%)';
 }
  
+function formatDate(date) {
+  const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = months[date.getMonth()];
+  const year = date.getFullYear().toString();
+  return `${day} ${month} ${year}`;
+}
 
-  function updateBlogDisplay(){
+async function updateBlogDisplay() {
+  try {
+      const response = await fetch("https://my-brand-backend-iyxk.onrender.com/api/blogs", {
+          method: "GET",
+      });
+      const data = await response.json();
+      allBlogs = data;
 
-    let blogs = '';
+      
+    for(let i=0; i<allBlogs.length; i++){
+     totalBlogs +=1;
+    }
+  
     
-  allBlogs.forEach((blog,index) =>{
-    blogs += `<div class="oneBlog">
-    <div class="imgDiv">
-        <img class="blogImage" src="${blog.bImage}">
-        <div class="blogDate blDate">${blog.bDate}</div>
-    </div>
-    <div class="details">
-        <div class="titl">${blog.bTitle}</div>
-        <div class="desc">${blog.bShortDesc}</div>
-        
-    </div>
-    <div class="buttons">
-        <button class="editBut edit-button" onclick="editBlog(${index})">Edit</button>
-        <button class="deleteBut" onclick="deleteBlog(${index})">Delete</button>
-        <div class="feedBlogs">
-                <div class="like-but js-like-but"><img class="thumb" src="../Images/Like.svg" alt=""></div>
-                <div class="numOfLiks">${blog.bNumOfLike}</div>
-                <button class="like-but js-comment-but" onclick="seeComments(${index})">
-                <img class="thumbs" src="../Images/Comment.svg" alt=""></button>
-            </div>
-    </div>
-  </div>`
-  
-  });
 
-  document.querySelector('.js-all-blogs').innerHTML = `<div class="allBlogs">
-                ${blogs}
-        </div>`;
+      let blogs = '';
+      allBlogs.forEach((blog, index) => {
 
-        attachEditButtonListeners();
+        const dateString = blog.bDate;
+        const dateObject = new Date(dateString);
+        let formated = formatDate(dateObject);
 
+
+          blogs += `<div class="oneBlog">
+              <div class="imgDiv">
+                  <img class="blogImage" src="${blog.bImage}">
+                  <div class="blogDate blDate">${formated}</div>
+              </div>
+              <div class="details">
+                  <div class="titl">${blog.bTitle}</div>
+                  <div class="desc">${blog.bShortDesc}</div>
+              </div>
+              <div class="buttons">
+                  <button class="editBut edit-button" onclick="editBlog(${index})">Edit</button>
+                  <button class="deleteBut" onclick="deleteBlog(${index})">Delete</button>
+                  <div class="feedBlogs">
+                      <div class="like-but js-like-but"><img class="thumb" src="../Images/Like.svg" alt=""></div>
+                      <div class="numOfLiks">${blog.bNumOfLike}</div>
+                      <button class="like-but js-comment-but" onclick="seeComments(${index})">
+                          <img class="thumbs" src="../Images/Comment.svg" alt="">
+                      </button>
+                  </div>
+              </div>
+          </div>`;
+
+          console.log(totalBlogs);
+      });
+
+      document.querySelector('.js-all-blogs').innerHTML = `<div class="allBlogs">${blogs}</div>`;
+      attachEditButtonListeners();
+
+      
+
+  } catch (error) {
+      console.error('Error:', error);
   }
+}
 
-  updateBlogDisplay();
-  
+updateBlogDisplay();
+
+
   function seeComments(index){
     let blogSeeComments = allBlogs[index];
     let allComs = '';
@@ -70,7 +100,10 @@ let allBlogs = JSON.parse(localStorage.getItem('allTheBlogs')) || [];
     console.log(blogComms);
     blogComms.forEach((comment, commentIndex) => {
       allComs += `<div class="oneComment">
-      <div class="comWord">${comment}</div>
+      <div class="commentData">
+        <div class="comSender">${comment.sender}</div>
+        <div class="comWord">${comment.comment}</div>
+       </div>
       <button class="delComBut js-del-com" onclick="deleteComment(${index}, ${commentIndex})">
 
           <img class="delImg" src="dImage/Bin.svg" alt="">
@@ -78,8 +111,10 @@ let allBlogs = JSON.parse(localStorage.getItem('allTheBlogs')) || [];
   </div>`
 
     })
-
     document.querySelector('.coms').innerHTML= `${allComs}`
+
+    adminCommentView.style.display = 'flex'
+
   }
 
   function deleteComment(blogIndex, commentIndex) {
@@ -93,9 +128,27 @@ let allBlogs = JSON.parse(localStorage.getItem('allTheBlogs')) || [];
 
   
   function deleteBlog(index) {
-    allBlogs.splice(index, 1);
+    let blogToDelete = allBlogs[index];
+    let id = blogToDelete._id;
+    const token = localStorage.getItem("token");
+
+    fetch(`https://my-brand-backend-iyxk.onrender.com/api/blogs/${id}`,{
+      method: "DELETE",
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    .then(async(res)=>{
+      const data = await res.json();
+      console.log(data.message);
+      if(data.message === "Blog deleted successfully"){
+        alert("Blog Have Been Deleted");
+      }
     updateBlogDisplay();
-    localStorage.setItem('allTheBlogs', JSON.stringify(allBlogs));
+    })
+    .catch(error => {
+      console.error('Error:', error);
+  });
   }
 
   function editBlog(index) {
@@ -112,59 +165,84 @@ let allBlogs = JSON.parse(localStorage.getItem('allTheBlogs')) || [];
     editBlogLongDescInput.value = blogToEdit.bLongDesc;
   
     saveEditsButton.onclick = function() {
+        const token = localStorage.getItem("token");
 
-        blogToEdit.bTitle = editBlogTitleInput.value;
-        blogToEdit.bShortDesc = editBlogShortDescInput.value;
-        blogToEdit.bLongDesc = editBlogLongDescInput.value;
-  
-        if (editBlogImageInput.files.length > 0) {
-            const newBlogImageFile = editBlogImageInput.files[0];
-            const reader = new FileReader();
-  
-            reader.onload = function(e) {
-
-                blogToEdit.bImage = e.target.result;
-  
-                updateBlogDisplay();
-  
-                editBlogs.style.display = 'none';
-                rightElement.style.filter = 'grayscale(0%)';
-                leftElement.style.filter = 'grayscale(0%)';
-
-                localStorage.setItem('allTheBlogs', JSON.stringify(allBlogs));
+            const updatedBlogData = {
+                bTitle: editBlogTitleInput.value,
+                bShortDesc: editBlogShortDescInput.value,
+                bLongDesc: editBlogLongDescInput.value,
             };
   
-            reader.readAsDataURL(newBlogImageFile);
-        } else {
+            if (editBlogImageInput.files.length > 0) {
+                const newBlogImageFile = editBlogImageInput.files[0];
+                const reader = new FileReader();
+  
+                reader.onload = function(e) {
+                    const blogImageBase = e.target.result;
+  
+                    updatedBlogData.bImage = blogImageBase;
+                    
+                    fetch(`https://my-brand-backend-iyxk.onrender.com/api/blogs/${blogToEdit._id}`, {
+                            method: "PUT",
+                            headers: {
+                              "Content-Type": "application/json",
+                              "Authorization": `Bearer ${token}`
+                            },
+                            body: JSON.stringify(updatedBlogData)
+                        }).then(async (res) => {
+                          const data = await res.json();
+                          console.log(data);
 
-            updateBlogDisplay();
+                          updateBlogDisplay();
+                          editBlogs.style.display = 'none';
+                          rightElement.style.filter = 'grayscale(0%)';
+                          leftElement.style.filter = 'grayscale(0%)';
+                        })
+                        .catch(error => {
+                          console.error('Error:', error);
+                      });
+                           
+                        
+                };
+                reader.readAsDataURL(newBlogImageFile);
+            } 
+            else {
+                    fetch(`https://my-brand-backend-iyxk.onrender.com/api/blogs/${blogToEdit._id}`, {
+                        method: "PUT",
+                        headers: {
+                          "Content-Type": "application/json",
+                          "Authorization": `Bearer ${token}`
+                        },
+                        body: JSON.stringify(updatedBlogData)
+                    }).then(async(res)=>{
+                        const data = await res.json();
+                        updateBlogDisplay();
+                        editBlogs.style.display = 'none';
+                        rightElement.style.filter = 'grayscale(0%)';
+                        leftElement.style.filter = 'grayscale(0%)';
+                    })
+                    .catch(error => {
+                      console.error('Error:', error);
+                  });
+                      
+                  }
+                        
+            }
+        } 
+    
 
-            editBlogs.style.display = 'none';
-            rightElement.style.filter = 'grayscale(0%)';
-            leftElement.style.filter = 'grayscale(0%)';
 
-            localStorage.setItem('allTheBlogs', JSON.stringify(allBlogs));
-        }
-    };
-  }
 
   const viewCommentButton = document.querySelectorAll('.js-comment-but');
 
   const adminCommentView = document.querySelector('.commentView');
   viewCommentButton.forEach(viewCom =>{
     viewCom.addEventListener('click', ()=>{
-      adminCommentView.style.display = 'flex'
+      
     })
   })
   function cancleBlogComment(){
     adminCommentView.style.display = 'none'
   }
 
-  const deleteCommentButton = document.querySelectorAll('.js-del-com');
-
-  deleteCommentButton.forEach(delComBut =>{
-    delComBut.addEventListener('click', ()=>{
-
-    })
-  })
-
+ 
