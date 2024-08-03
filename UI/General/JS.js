7c5b6d4b914ec6d82a4e21910c9befac2c4d6733
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2024-08-03 13:27:59 +0200

    Implement changing theme

--- UI/General/JS.js
+++ UI/General/JS.js
@@ -1,43 +1,65 @@
-let loader = document.querySelector('.loaderContainer');
-function showLoader(){
+let loader = document.querySelector(".loaderContainer");
+function showLoader() {
   loader.style.display = "flex";
 }
 
-function hideLoader(){
+function hideLoader() {
   loader.style.display = "none";
 }
 
-console.log(localStorage.getItem('isLoggedIn'));
+console.log(localStorage.getItem("isLoggedIn"));
 
-let messageSent = document.querySelector('.messageSent');
+let messageSent = document.querySelector(".messageSent");
 
 function showToast(message, type) {
   let toastDiv;
-  if (type === 'success') {
+  if (type === "success") {
     toastDiv = `<img src="../UI/Images/Checked.svg" class="check-img js-toast" alt=""> <div>${message}</div>`;
     messageSent.innerHTML = toastDiv;
-    messageSent.style.display = 'flex';
+    messageSent.style.display = "flex";
     setTimeout(() => {
-      messageSent.style.display = 'none';
+      messageSent.style.display = "none";
     }, 2000);
   } else {
     toastDiv = `<div>${message}</div>`;
-    messageSent.style.backgroundColor = 'rgb(253, 114, 114)';
+    messageSent.style.backgroundColor = "rgb(253, 114, 114)";
     messageSent.innerHTML = toastDiv;
-    messageSent.style.display = 'flex';
+    messageSent.style.display = "flex";
     setTimeout(() => {
-      messageSent.style.display = 'none';
+      messageSent.style.display = "none";
     }, 2000);
   }
 }
 
-document.querySelectorAll('.logout-btn').forEach(button => {
-  button.addEventListener('click', function() {
-      localStorage.removeItem('token');
-      window.location.href = '../index.html';
+document.querySelectorAll(".logout-btn").forEach((button) => {
+  button.addEventListener("click", function () {
+    localStorage.removeItem("token");
+    window.location.href = "../index.html";
   });
 });
 
+const themeIcon = document.querySelector(".js-theme-icon");
+const body = document.body;
+const logoDiv = document.querySelector(".js-logo");
 
+let theme = localStorage.getItem("theme") || "light";
 
+function applyTheme(theme) {
+  if (theme === "dark") {
+    themeIcon.innerHTML = "<i title='Light Mode' class='fa-solid fa-star'></i>";
+    body.classList.add("dark");
+    logoDiv.innerHTML='<img class="logo-name" src="Images/Caleb’s Brand (1).svg" alt="" />'
+  } else {
+    themeIcon.innerHTML = "<i title='Night Mode' class='fa-solid fa-moon'></i>";
+    body.classList.remove("dark");
+    logoDiv.innerHTML='<img class="logo-name" src="Images/Caleb’s Brand (2).svg" alt="" />'
+  }
+}
 
+applyTheme(theme);
+
+themeIcon.addEventListener("click", () => {
+  theme = theme === "light" ? "dark" : "light";
+  localStorage.setItem("theme", theme);
+  applyTheme(theme);
+});
--- UI/Portfolio.js
+++ UI/Portfolio.js
@@ -10,95 +10,95 @@ function BlogDisplay() {
       hideLoader();
       allBlogs = data;
 
-      let landingBlogs = ``;
-      allBlogs.forEach((aBlog, index) => {
-        const dateString = aBlog.bDate;
-        const dateObject = new Date(dateString);
-        let formatted = formatDate(dateObject);
+      renderBlogs(allBlogs);
 
-        landingBlogs += `
-          <div class="swiper-slide">
-            <div class="blogImage">
-              <img class="blogImg" src="${aBlog.bImage}" alt="">
-            </div>
-            <div class="blogDate">${formatted}</div>
-            <div class="bloggTitle">${aBlog.bTitle}</div>
-            <div class="justifyBlog">
-              <div class="blogDesc">${aBlog.bShortDesc}</div>
-              <div class="blogInteractions">
-                <div class="blogFeedback">
-                  <button class="like-but js-like-but lik-but-${index}" onclick="handleLikeButtonClick(${index}, '${
-          aBlog._id
-        }')">
-                    <div>
-                      <img class="thumb" src="${
-                        isBlogLiked(aBlog._id)
-                          ? "Images/Liked.svg"
-                          : "Images/Lik.svg"
-                      }" alt="">
-                    </div>
-                  </button>
-                  <div class="numOfLiks js-num-likes-${index}">${
-          aBlog.bNumOfLike
-        }</div>
-                  <button class="like-but js-comment-button" onclick="viewAndScroll(${index})">
-                    <img class="thumb" src="Images/Comment.svg" alt="">
-                  </button>
-                </div>
-                <button onclick="viewBlogDetails(${index})" class="blogBut js-blog-but">
-                  View More <img src="Images/rightt arrow.svg" alt="">
-                </button>
-              </div>
-            </div>
-          </div>
-`;
-      });
-
-      const blogsWrapper = document.querySelector(".js-blogs");
-      if (blogsWrapper) {
-        blogsWrapper.innerHTML = `<div class="card-wrapper swiper-wrapper">${landingBlogs}</div>`;
-
-        if (allBlogs.length > 1) {
-          var swiper = new Swiper(".slide-content", {
-            slidesPerView: 3,
-            spaceBetween: 30,
-            loop: true,
-            fade: true,
-            grabCursor: true,
-            keyboard: {
-              enabled: true,
-            },
-            scrollbar: {
-              el: ".swiper-scrollbar",
-            },
-            navigation: {
-              nextEl: ".swiper-button-next",
-              prevEl: ".swiper-button-prev",
-            },
-            pagination: {
-              el: ".swiper-pagination",
-              clickable: true,
-              dynamicBullets: true,
-            },
-            breakpoints: {
-              0: {
-                slidesPerView: 1,
-              },
-              550: {
-                slidesPerView: 2,
-              },
-              920: {
-                slidesPerView: 3,
-              },
-            },
-          });
-        }
-      }
       blogButton = document.querySelectorAll(".js-blog-but");
     })
     .catch((error) => {
       console.error("Error:", error);
     });
+}
+
+function renderBlogs(blogs) {
+  let landingBlogs = ``;
+  blogs.forEach((aBlog, index) => {
+    const dateString = aBlog.bDate;
+    const dateObject = new Date(dateString);
+    let formatted = formatDate(dateObject);
+
+    landingBlogs += `
+      <div class="swiper-slide">
+        <div class="blogImage">
+          <img class="blogImg" src="${aBlog.bImage}" alt="">
+        </div>
+        <div class="blogDate">${formatted}</div>
+        <div class="bloggTitle">${aBlog.bTitle}</div>
+        <div class="justifyBlog">
+          <div class="blogDesc">${aBlog.bShortDesc}</div>
+          <div class="blogInteractions">
+            <div class="blogFeedback">
+              <button class="like-but js-like-but lik-but-${index}" onclick="handleLikeButtonClick(${index}, '${aBlog._id}')">
+                <div>
+                  <img class="thumb" src="${isBlogLiked(aBlog._id) ? "Images/Liked.svg" : "Images/Lik.svg"}" alt="">
+                </div>
+              </button>
+              <div class="numOfLiks js-num-likes-${index}">${aBlog.bNumOfLike}</div>
+              <button class="like-but js-comment-button" title='Comments' onclick="viewAndScroll(${index})">
+                <i class="fa-solid fa-comment thumb-comment"></i>
+              </button>
+            </div>
+            <button onclick="viewBlogDetails(${index})" class="blogBut js-blog-but">
+              View More <i class="fa-solid fa-arrow-right"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  const blogsWrapper = document.querySelector(".js-blogs");
+  if (blogsWrapper) {
+    blogsWrapper.innerHTML = `<div class="card-wrapper swiper-wrapper">${landingBlogs}</div>`;
+    initializeSwiper(blogs.length);
+  }
+}
+
+function initializeSwiper(blogCount) {
+  if (blogCount > 1) {
+    var swiper = new Swiper(".slide-content", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop: true,
+      fade: true,
+      grabCursor: true,
+      keyboard: {
+        enabled: true,
+      },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        550: {
+          slidesPerView: 2,
+        },
+        920: {
+          slidesPerView: 3,
+        },
+      },
+    });
+  }
 }
 
 function isBlogLiked(blogId) {
@@ -111,44 +111,36 @@ async function toggleLike(index, blogId) {
   const likeButton = document.querySelector(`.lik-but-${index}`);
   const thumbImage = likeButton.querySelector(".thumb");
 
-  if (isBlogLiked(blogId)) {
-    try {
-      showLoader();
-      const response = await fetch(
-        `https://my-brand-backend-iyxk.onrender.com/api/blogs/${blogId}/disLike`,
-        {
-          method: "POST",
-        }
-      );
-      const data = await response.json();
-      hideLoader();
-    } catch (error) {
-      console.error("Error:", error);
-    }
+  try {
+    showLoader();
+    const url = isBlogLiked(blogId)
+      ? `https://my-brand-backend-iyxk.onrender.com/api/blogs/${blogId}/disLike`
+      : `https://my-brand-backend-iyxk.onrender.com/api/blogs/${blogId}/like`;
 
-    const updatedLikes =
-      parseInt(likeButton.nextElementSibling.textContent) - 1;
+    const response = await fetch(url, {
+      method: "POST",
+    });
+
+    const data = await response.json();
+    hideLoader();
+
+    updateLikeStatus(isBlogLiked(blogId), likeButton, thumbImage, index, blogId);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+function updateLikeStatus(isLiked, likeButton, thumbImage, index, blogId) {
+  const likedBlogs = JSON.parse(localStorage.getItem("likedBlogs")) || [];
+
+  if (isLiked) {
+    const updatedLikes = parseInt(likeButton.nextElementSibling.textContent) - 1;
     likeButton.nextElementSibling.textContent = updatedLikes;
     thumbImage.src = "Images/Lik.svg";
     const updatedLikedBlogs = likedBlogs.filter((id) => id !== blogId);
     localStorage.setItem("likedBlogs", JSON.stringify(updatedLikedBlogs));
   } else {
-    try {
-      showLoader();
-      const response = await fetch(
-        `https://my-brand-backend-iyxk.onrender.com/api/blogs/${blogId}/like`,
-        {
-          method: "POST",
-        }
-      );
-      const data = await response.json();
-      hideLoader();
-    } catch (error) {
-      console.error("Error:", error);
-    }
-
-    const updatedLikes =
-      parseInt(likeButton.nextElementSibling.textContent) + 1;
+    const updatedLikes = parseInt(likeButton.nextElementSibling.textContent) + 1;
     likeButton.nextElementSibling.textContent = updatedLikes;
     thumbImage.src = "Images/Liked.svg";
     likedBlogs.push(blogId);
@@ -219,7 +211,7 @@ function hideNameInput() {
   }
 }
 
-function viewBlogDetails(index) {
+function viewBlogDetails(index, shouldScroll = false) {
   index = index || 0;
   const blogToView = allBlogs[index];
   let id = blogToView._id;
@@ -239,93 +231,94 @@ function viewBlogDetails(index) {
       allComments = data;
       allComments.forEach((comment) => {
         commentsHTML += `
-                <div class="oneCom">
-                    <div class="comDetails">
-                        <div class="comSender">${comment.sender}</div>
-                        <div>${comment.comment}</div>
-                    </div>
-                </div>
-            `;
+          <div class="oneCom">
+              <div class="comDetails">
+                  <div class="comSender">${comment.sender}</div>
+                  <div>${comment.comment}</div>
+              </div>
+          </div>
+        `;
       });
 
       const blogDetails = `
       <div class="singleBlogDiv">
-<div class="topContainer">
-  <div class="cancleDiv">
-    <button class="canelBut" onclick="cancleViewBlog()">x</button>
-  </div>
-</div>
-
-<div class="blogLeft">
-  <div class="topDesc">
-    <img class="blImage" src="${blogToView.bImage}" alt="" />
-    <div class="bllDesc">
-      <div class="blTitle">${blogToView.bTitle}</div>
-      <div class="blDesc">${blogToView.bShortDesc}</div>
-      <div class="blDate">${formatted}</div>
-      <div class="blIcon">
-        <button
-          class="like-but js-like-but lik-but-${index}"
-          onclick="handleLikeButtonClick(${index}, '${blogToView._id}')"
-        >
-          <div>
-            <img class="thumb" src="${
-              isBlogLiked(blogToView._id)
-                ? "Images/Liked.svg"
-                : "Images/Lik.svg"
-            }" alt="">
+        <div class="topContainer">
+          <div class="cancleDiv">
+            <button class="canelBut" onclick="cancleViewBlog()">x</button>
           </div>
-        </button>
-        <div class="numOfLiks js-num-likes-${index}">
-          ${blogToView.bNumOfLike}
         </div>
-        <button
-          class="like-but js-comment-button"
-          onclick="scrollToComments(${index})"
-        >
-          <img class="com-but" src="Images/Comment.svg" alt="" />
-        </button>
-      </div>
-    </div>
-  </div>
-  <div class="botDesc">${blogToView.bLongDesc}</div>
-</div>
-
-<div class="blogRight">
-  <div id="comments-${index}" class="botComment">
-    <div class="comHead">Comment Section</div>
-    <div class="comments">
-      <div class="commentHTML">${commentsHTML}</div>
-    </div>
-    <form action="">
-      <div class="writer">
-        <div class="commentSenderinputs">
-            <input
-            required
-            type="text"
-            name="comment"
-            class="write js-write-comment"
-            placeholder="Add Comment ..."
-            />
-            <input
-            required
-            type="text"
-            name="sender"
-            class="write-name js-write-name"
-            placeholder="Your Name"
-            />
+      
+        <div class="blogLeft">
+          <div class="topDesc">
+            <img class="blImage" src="${blogToView.bImage}" alt="" />
+            <div class="bllDesc">
+              <div class="blTitle">${blogToView.bTitle}</div>
+              <div class="blDesc">${blogToView.bShortDesc}</div>
+              <div class="blDate">${formatted}</div>
+              <div class="blIcon">
+                <button
+                  class="like-but js-like-but lik-but-${index}"
+                  onclick="handleLikeButtonClick(${index}, '${blogToView._id}')"
+                >
+                  <div>
+                    <img class="thumb" src="${
+                      isBlogLiked(blogToView._id)
+                        ? "Images/Liked.svg"
+                        : "Images/Lik.svg"
+                    }" alt="">
+                  </div>
+                </button>
+                <div class="numOfLiks js-num-likes-${index}">
+                  ${blogToView.bNumOfLike}
+                </div>
+                <button
+                title='Comments'
+                  class="like-but js-comment-button"
+                  onclick="scrollToComments(${index})"
+                >
+                <i class="fa-solid fa-comment thumb-comment"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="botDesc">${blogToView.bLongDesc}</div>
         </div>
-        <button
-          class="someButs js-send-comment"
-          onclick="sendComment(event, ${index})"
-        >
-          <img class="blSend" src="Images/Send.svg" alt="" />
-        </button>
+      
+        <div class="blogRight">
+          <div id="comments-${index}" class="botComment">
+            <div class="comHead">Comment Section</div>
+            <div class="comments">
+              <div class="commentHTML">${commentsHTML}</div>
+            </div>
+            <form action="">
+              <div class="writer">
+                <div class="commentSenderinputs">
+                    <input
+                    required
+                    type="text"
+                    name="comment"
+                    class="write js-write-comment"
+                    placeholder="Add Comment ..."
+                    />
+                    <input
+                    required
+                    type="text"
+                    name="sender"
+                    class="write-name js-write-name"
+                    placeholder="Your Name"
+                    />
+                </div>
+                <button
+                  class="someButs js-send-comment"
+                  onclick="sendComment(event, ${index})"
+                >
+                <i title='Send Comment' class="fa-regular fa-paper-plane blSend"></i>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-    </form>
-  </div>
-</div>
-</div>
       `;
 
       document.querySelector(".js-view-blog-Detailed").innerHTML = blogDetails;
@@ -346,6 +339,10 @@ function viewBlogDetails(index) {
         nameInput.addEventListener("focus", showNameInput);
         nameInput.addEventListener("blur", hideNameInput);
       }
+
+      if (shouldScroll) {
+        scrollToComments(index);
+      }
     })
     .catch((error) => {
       console.error("Error:", error);
@@ -353,8 +350,7 @@ function viewBlogDetails(index) {
 }
 
 function viewAndScroll(index) {
-  viewBlogDetails(index);
-  scrollToComments(index);
+  viewBlogDetails(index, true);
 }
 
 function scrollToComments(index) {
@@ -376,7 +372,7 @@ const closeBtn = document.getElementById("closeBtn");
 
 let showMobileBar = false;
 
-document.querySelectorAll(".menu-link").forEach((anchor) => {
+document.querySelectorAll(".js-menu-link").forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
 
